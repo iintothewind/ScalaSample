68857e42a982050fba4869202812a77429b11a9d
ivar <iintothewind@yeah.net>
2016-08-11 21:14:02 +0800

    update state sample

--- src/test/scala/fp/intro/StateSample.scala
+++ src/test/scala/fp/intro/StateSample.scala
@@ -147,9 +147,9 @@ case object Turn extends Input
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object Machine {
-  //  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
-  //    ???
-  //  }
+    def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+      ???
+    }
 }
 
 
