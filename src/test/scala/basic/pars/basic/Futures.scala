1418a9fba5eb137a33790cb30baebda5619dcb45
yinghuachen <iintothewind@yeah.net>
2018-03-01 00:11:15 +0800

    Futures: added test firstCompletedOf .gitignore: added vscode

--- src/test/scala/basic/pars/basic/Futures.scala
+++ src/test/scala/basic/pars/basic/Futures.scala
@@ -11,21 +11,30 @@ import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 /**
-http://docs.scala-lang.org/overviews/core/futures
+  * http://docs.scala-lang.org/overviews/core/futures
   */
 class Futures {
 
   @Rule
   def watcher: TestWatcher = new TestWatcher {
+    override def starting(description: Description): Unit = println(s"testing method: ${description.getMethodName}()")
+
     override def succeeded(description: Description): Unit = TimeUnit.SECONDS.sleep(1)
   }
 
   @Test
-  def testFuture(): Unit = {
+  def testFirstCompletedOf(): Unit = {
     val msg = Future {
-      TimeUnit.MILLISECONDS.sleep(10)
-      "test"
-    }.onComplete {
+      TimeUnit.MILLISECONDS.sleep(100)
+      "msg1 test"
+    }
+
+    val msg2 = Future {
+      TimeUnit.MILLISECONDS.sleep(90)
+      throw new RuntimeException("sth wrong")
+    }
+
+    Future.firstCompletedOf(Seq(msg, msg2)).onComplete {
       case Success(s) => println(s)
       case Failure(e) => println(e.getMessage)
     }
